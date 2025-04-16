cd600312192869c40c271803df46c67a38fef6e0
shahnoor77 <shahnoorkhan9955@gmail.com>
2025-04-16 21:44:18 +0500

    updated feature group version

--- services/kafka_to_feature_store/setup_historical_config.sh
+++ services/kafka_to_feature_store/setup_historical_config.sh
@@ -1,7 +1,7 @@
 export KAFKA_TOPIC=ohlc_historical
 export KAFKA_CONSUMER_GROUP=ohlc_historical_consumer_group_NEW
 export FEATURE_GROUP_NAME=ohlc_feature_group
-export FEATURE_GROUP_VERSION=1
+export FEATURE_GROUP_VERSION=3
 
 # number of elements we save at once to the Hopsworks feature store
 # This value of 10080 corresponds to saving batches of 1 week of data at once
