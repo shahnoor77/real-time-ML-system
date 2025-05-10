dc4759e5993eb066ef8cc0c5192fcc0d04cae010
shahnoor77 <shahnoorkhan9955@gmail.com>
2025-05-10 21:44:03 +0500

    update

--- services/price_predictor/README.md
+++
@@ -1,9 +0,0 @@
-steps:
-1 - Fetch ohlc_data from FeatureStore.
-2 - Split the data into Train and Test data.
-3 - Preprocess data 
-    Add missing rows, replace NaN values, etc.
-4 - Create a Target metric 
-5 - Train the Model
- Next step:
- --push the model to model registry using COMET-ML
--- services/price_predictor/src/utils.py
+++
@@ -1,9 +0,0 @@
-def get_model_name(product_id: str) -> str:
-    """
-    Get the model name from given product_id.
-    Args:
-        product_id (str): The product ID.
-    Returns:
-        str: The model name.
-    """    
-    return f'{product_id.replace("/", "_")}_price_change_predictor'
